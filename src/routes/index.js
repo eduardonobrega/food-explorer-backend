7d4f7acc6eabc1328c6f71b6190be9e16373b874
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-27 10:48:23 -0300

    dishes routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
 
 const usersRouter = require('./users.routes');
+const dishesRouter = require('./dishes.routes');
 
 const router = Router();
 router.use('/users', usersRouter);
+router.use('/dishes', dishesRouter);
+
 router.get('/', (request, response) =>
   response.send(`<h1>Welcome API FoodExplorer</h1>`)
 );
