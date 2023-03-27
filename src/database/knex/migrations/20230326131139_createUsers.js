9e0e4557e37d216fbb185d5ae021365821daf696
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-27 10:47:52 -0300

    create user

---
+++ src/database/knex/migrations/20230326131139_createUsers.js
@@ -0,0 +1,12 @@
+exports.up = (knex) =>
+  knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+
+    table.text('name').notNullable();
+    table.text('email').notNullable();
+    table.text('password').notNullable();
+
+    table.boolean('isAdmin').defaultTo(false);
+  });
+
+exports.down = (knex) => knex.schema.dropTable('users');
