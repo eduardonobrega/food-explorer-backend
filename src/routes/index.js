3385947693763becc0520996181a6fd3fe89db96
Eduardo Nóbrega <eduardo.nobrega1999@hotmail.com>
2023-03-29 09:22:55 -0300

    user token

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,15 @@ const dishesRouter = require('./dishes.routes');
 const favoritesRouter = require('./favorites.routes');
 const requestsRouter = require('./requests.routes');
 const purchasesRouter = require('./purchases.routes');
-2
+const sessionsRouter = require('./sessions.routes');
+
 const router = Router();
 router.use('/users', usersRouter);
 router.use('/dishes', dishesRouter);
 router.use('/favorites', favoritesRouter);
 router.use('/requests', requestsRouter);
 router.use('/purchases', purchasesRouter);
+router.use('/sessions', sessionsRouter);
 
 router.get('/', (request, response) =>
   response.send(`<h1>Welcome API FoodExplorer</h1>`)
